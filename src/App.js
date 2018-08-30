854e19fed4dcdb82e261d49ee1fbb6a681bcb745
Stephane Mensah <stephane.mensah@st.vvu.edu.gh>
2018-08-30 19:38:01 +0000

    Created the components and added react-icons components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Classroom from './components/classroom/Classroom';
 
-class CountDown extends Component {
-  
-}
+//icons
+import { FaGithubAlt } from 'react-icons/fa';
+
+// global var
+    const author_name = "Waptik";
+    const github = "https://www.github.com/";
+    //const author_url = github + "waptik";
 
 class App extends Component {
 
+  
+// initialising my state
   state = {
-      survey:[
-        
-      ],
-      students: [
-        {
-          name: "Stephane",
-          age: 22,
+      author_name: "Waptik",
+      author_url: github + "waptik",
+      seconds: '00',
+      minutes: '00',
+      hours: '00'
   }
-      ]
-  }
+
+
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">ClockDown by <a href={this.state.author_url} target="_blank">Waptik</a><FaGithubAlt /></h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
-      <div className="classroom">
-          <Classroom students={this.state.students} />
-      </div>
 
       </div>
     );
---
+++ src/components/Buttons.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+class Buttons extends Component {
+    static propTypes = {
+        className: PropTypes.string,
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default Buttons;
